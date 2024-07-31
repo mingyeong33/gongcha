afe7dca749c01c3372ee02eec08eace130188c2d
mingyeong33 <168386242+mingyeong33@users.noreply.github.com>
2024-07-31 14:07:27 +0900

    Add files via upload

---
+++ script/main.js
@@ -0,0 +1,116 @@
+$(function () {
+    $("header nav .menu>li").mouseover(function () {
+        $("nav .sub").stop().slideDown();
+    });
+    $("header nav .menu>li").mouseout(function () {
+        $("nav .sub").stop().slideUp();
+    });
+
+
+    $(".ham").click(function () {
+        $(".m-menu").stop().animate({ right: '0px' });
+    });
+
+    $(".close").click(function () {
+        $(".m-menu").stop().animate({ right: '-200px' });
+    });
+
+   /*  $(".m-menu nav ul>li").click(function () {
+        $(".m-menu .m_submenu").stop().slideDown();
+    });
+      $(".m-menu nav ul>li").click(function(){
+          $(".m-menu .m_submenu").stop().slideUp();
+      }); */
+
+
+    $('.best .c_group').slick();
+
+    /* section3 */
+       /*  $('.hto .box').slick({
+            slidesToShow: 2,
+            slidesToScroll: 1,
+            mobileFirst: true,
+            arrows: false,
+            dots: true,
+            responsive: [
+                {
+                    breakpoint: 1500,
+                    settings: 'unslick'
+                }
+            ]
+        });
+        $('.hto .box').slick({
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            mobileFirst: true,
+            arrows: false,
+            dots: true,
+            responsive: [
+                {
+                    breakpoint: 800,
+                    settings: 'unslick'
+                }
+            ]
+        });
+     
+        $(window).on('resize', function() {
+            $('.hto .box').slick('resize');
+        }); */
+    /* section3 */
+
+    /* section4 */
+    $(".tr .text .t1").on({
+        click: function () {
+            $('.tr .box1').css({
+                display: 'block'
+            });
+        },
+        click: function () {
+            $(".tr .box2").css({
+                display: 'none'
+            });
+        },
+        click: function () {
+            $(".tr .box3").css({
+                display: 'none'
+            });
+        }
+    });
+
+    $(".tr .text .t2").on({
+        click: function () {
+            $('.tr .box1').css({
+                display: 'none'
+            });
+        },
+        click: function () {
+            $(".tr .box2").css({
+                display: 'block'
+            });
+        },
+        click: function () {
+            $(".tr .box3").css({
+                display: 'none'
+            });
+        }
+    });
+
+    $(".tr .text .t3").on({
+        click: function () {
+            $('.tr .box1').css({
+                display: 'none'
+            });
+        },
+        click: function () {
+            $(".tr .box2").css({
+                display: 'none'
+            });
+        },
+        click: function () {
+            $(".tr .box3").css({
+                display: 'block'
+            });
+        }
+    });
+    /* //section4 */
+});
